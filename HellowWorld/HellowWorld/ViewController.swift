d388458ba80f73a571276e2a4befb556cae7a913
AlexeiPozdnyakov <pozdnyakov.alexei@gmail.com>
2022-04-07 23:12:33 +0700

    HelloWorld

---
+++ HellowWorld/HellowWorld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HellowWorld
+//
+//  Created by Alexei Pozdnyakov on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        print("Hello World!!!!")
+    }
+
+
+}
+
